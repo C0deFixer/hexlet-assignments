d2f73bad98d85a146d3e5dacc7da3432fa53d0e3
@hexlet/cli <support@hexlet.io>
2023-09-07 14:26:50 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -7,21 +7,26 @@ import java.util.StringJoiner;
 
 // BEGIN
 public class App {
-public static void main(String[]args){
+    public static void main(String[] args) {
     /*Map<String, String> mp = new HashMap<>();
     Map<String, String> lhm = new LinkedHashMap<>();
     mp.put("test", "code");
     System.out.println(mp);*/
-    Map<String, Integer> hmp = getWordCount("word text cat apple word map apple word");
-    System.out.println(hmp);
-    System.out.println(toString(hmp));
-        }
+        Map<String, Integer> hmp = getWordCount("word text cat apple word map apple word");
+        System.out.println(hmp);
+        System.out.println(toString(hmp));
+        hmp = getWordCount("");
+        System.out.println(toString(hmp));
+    }
 
     public static Map<String, Integer> getWordCount(String sentence) {
         String[] words = sentence.trim().split(" ");
         Map<String, Integer> resultMap = new HashMap<String, Integer>(words.length);
         for (String word : words) {
-            if (word.length() != 0 && resultMap.containsKey(word)) {
+            if (word == null || word.length() == 0) {
+                continue;
+            }
+            else if (resultMap.containsKey(word)) {
                 int oldvalue = resultMap.get(word).intValue();
                 resultMap.put(word, Integer.valueOf((oldvalue + 1)));
             } else {
@@ -35,7 +40,8 @@ public static void main(String[]args){
     public static String toString(Map<String, Integer> hmp) {
         if (hmp.isEmpty()) {
             return "{}";
-        } ;
+        }
+        ;
         StringJoiner result = new StringJoiner("");
         result.add("{");
         for (Map.Entry node : hmp.entrySet()) {
