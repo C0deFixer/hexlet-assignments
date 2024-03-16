b31de23797d1cd5ef08a10ef985d3326617c5287
@hexlet/cli <support@hexlet.io>
2024-03-16 20:03:14 +0300

    submit java-web-ru/mvc

---
+++ java-web-ru/mvc/src/main/java/exercise/App.java
@@ -0,0 +1,36 @@
+package exercise;
+
+import io.javalin.Javalin;
+import exercise.controller.PostsController;
+import exercise.controller.RootController;
+import exercise.util.NamedRoutes;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get(NamedRoutes.rootPath(), RootController::index);
+
+        app.get(NamedRoutes.buildPostPath(), PostsController::build);
+        app.post(NamedRoutes.postsPath(), PostsController::create); // - > "/posts"
+
+        app.get(NamedRoutes.postsPath(), PostsController::index);
+        app.get(NamedRoutes.postPath("{id}"), PostsController::show);
+
+        // BEGIN
+        app.get(NamedRoutes.editPath("{id}"), PostsController::edit); // -> "/posts/" + id + "/edit"  "posts/edit.jte"  EditPostPage
+        app.post(NamedRoutes.editPostPath("{id}"), PostsController::update); // - > "/posts/"+ id   "posts/edit.jte"  EditPostPage
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start("127.0.0.1", 8080);
+    }
+}
